74440a929b1612780bbd672d4095c674efa740de
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2017-02-26 16:15:53 +0100

    Write json data on submit

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var path = require('path');
 var jsonfile = require('jsonfile');
 
 var app = express();
-var file = './public/config.json';
+var output_file = 'public/config.json';
 
 var buttonConfig = {
     'uuid': 'Your button\'s UUID',
     'buttonName': 'Your button\'s name',
-    'recipient': 'Your friend\'s Telegram username', 
+    'recipient': 'Your friend\'s Telegram username',
     'shortPress': [],
     'longPress': [],
 };
@@ -25,42 +25,65 @@ app.use(express.static(path.join(__dirname, 'public')));
 var routes = function() {
     router.get('/',
     function(req, res) {
-        res.sendFile('./public/index.html');
+        console.log("got");
+        res.sendFile('public/index.html');
     });
 
     router.post('/',
     function(req, res) {
-        getConfig();
+        getConfig(output_file, req);
+        res.redirect('/');
     });
     return router;
 };
 
 app.use('/',routes());
 
-app.listen(3000, function () {
-    console.log('Listening');
+app.listen(4999, function () {
+    console.log('Listening on port 4999');
 });
 
-function getConfig(file) {
-    if (!jsonfile.readFile(file)) {
-        jsonfile.writeFile(file, buttonConfig, function(err) {
-            console.log(err);
-        })};
-    var name = req.body.name;
-    var uuid = req.body.uuid;
-    var recipient = req.body.recipient;
-    var long = req.body.long.replace("; ", ";").split(";");
-    var short = req.body.short.replace("; ", ";").split(";");
+function getConfig(file, req) {
+  jsonfile.readFile(file, function(err, obj) {
+    if(err) {
+      jsonfile.writeFile(file, buttonConfig, function(err) {
+        var name = req.body.name;
+        var uuid = req.body.uuid;
+        var recipient = req.body.recipient;
+        var long = req.body.long.replace("; ", ";").split(";");
+        var short = req.body.short.replace("; ", ";").split(";");
 
-    var newButtonConfig = {
+        var newButtonConfig = {
+          'uuid': uuid,
+          'buttonName': name,
+          'recipient': recipient,
+          'shortPress': short,
+          'longPress': long,
+        };
+
+        jsonfile.writeFile(file, newButtonConfig, function(err) {
+          console.log(err);
+        });
+      });
+    }
+    else {
+      var name = req.body.name;
+      var uuid = req.body.uuid;
+      var recipient = req.body.recipient;
+      var long = req.body.long.replace("; ", ";").split(";");
+      var short = req.body.short.replace("; ", ";").split(";");
+
+      var newButtonConfig = {
         'uuid': uuid,
         'buttonName': name,
         'recipient': recipient,
         'shortPress': short,
         'longPress': long,
-    };
-    jsonfile.writeFile(file, newButtonConfig, function(err) {
-        console.log(err);
-    });
-};
+      };
 
+      jsonfile.writeFile(file, newButtonConfig, function(err) {
+        console.log(err);
+      });
+    }
+  });
+};
